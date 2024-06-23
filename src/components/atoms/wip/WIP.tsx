3b945a30f3b0cc15419b938737d13ebcddca9567
arunanksharan <arunank.sharan@gmail.com>
2024-06-23 19:13:27 +0530

    modified wip label to appear on top left

--- src/components/atoms/wip/WIP.tsx
+++ src/components/atoms/wip/WIP.tsx
@@ -10,8 +10,8 @@ const Overlay: React.FC<OverlayProps> = ({ children, isWip }) => {
     <div className={`relative ${isWip ? 'pointer-events-none' : ''}`}>
       {children}
       {isWip && (
-        <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center z-10">
-          <p className="text-white text-xl bg-showcastNavbar px-12 py-2">
+        <div className="absolute inset-0 bg-black bg-opacity-25 flex items-center justify-center z-10">
+          <p className="text-white text-xl bg-showcastNavbar px-12 py-2 absolute top-20 left-0 transform -rotate-12 origin-top-left ">
             This app is under development!
           </p>
         </div>
